4163d093a579e737bd3b6f8de2bc36789742521c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-06 20:23:21 +0900

    add header

---
+++ components/header.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary text-light">
+    <a class="navbar-brand" href="#">Chat's!</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="Navber">
+        <ul class="navbar-nav ml-auto text-left mr-5">
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    メニュー
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">トップ</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">概要</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">チャット</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">その他</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
